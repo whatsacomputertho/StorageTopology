a20d0d1b2a9a58674a00d02d9a4ca6b35a433063
ethanbalcik <emb71@psu.edu>
2022-03-21 18:42:54 -0400

    Added example scripts for chapter 2 example

--- example-scripts/aws-example.py
+++ example-scripts/aws-example.py
@@ -6,13 +6,23 @@ from itertools import combinations
 
 #Specify latencies & capacities
 caps = [1, 1, 1, 1]
+
+#Note that edges are not directed
+#Instead took average of directed edges between nodes
 lats = [129.1, 246.0, 132.42, 122.1, 194.8, 100.5]
 
-#Check every replication scheme
+#Instantiate the network
 topology = StorageTopology(capacities=caps, num_nodes=4, num_objects=3, weights=lats)
+
+#Generate all allocations
 allocations = topology.generate_feasile_replicative_allocations()
+
+#Instantiate average latency variable and allocation
 avg_lat = numpy.inf
 alloc = []
+
+#Loop through all allocations and find the optimal one with regard to average latency
+#Finally print the optimal allocation
 for allocation in allocations:
     topology.allocate_coded_objects(allocation)
     latency = topology.calculate_global_average_latency()
---
+++ example-scripts/example-2-3.py
@@ -0,0 +1,36 @@
+import numpy as np
+from StorageTopology.StorageTopology import StorageTopology
+
+#Specify edge latencies
+latencies = [1.0, 2.0, 1.0, 1.0, 2.0, 1.0]
+
+#Instantiate the network
+topology = StorageTopology(num_nodes=4, num_objects=3, capacities=[1, 1, 1, 1], weights=latencies)
+
+#Generate all replicative allocations
+allocations = topology.generate_feasile_replicative_allocations()
+
+#Instantiate average and worst-case latency variable & allocation variable
+wc_lat = np.inf
+avg_lat = np.inf
+alloc = []
+
+#Minimize by average latency
+for allocation in allocations:
+    topology.allocate_coded_objects(allocation)
+    alat = topology.calculate_global_average_latency()
+    wlat = topology.calculate_global_worst_case_latency()
+    if alat < avg_lat:
+        avg_lat = alat
+        alloc = allocation
+    if wlat < wc_lat:
+        wc_lat = wlat
+
+#Check if allocation is minimal with regard to both worst-case and average latency
+topology.allocate_coded_objects(alloc)
+if topology.calculate_global_average_latency() == avg_lat:
+    print("Allocation is optimal with regard to average latency")
+    print("Average Latency: " + str(avg_lat))
+if topology.calculate_global_worst_case_latency() == wc_lat:
+    print("Allocation is optimal with regard to worst-case latency")
+    print("Wrost-Case Latency: " + str(wc_lat))
